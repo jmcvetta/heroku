0c1eb83c436fba739dc4c5ddd32332818aa50406
Jason McVetta <jason.mcvetta@gmail.com>
2013-05-01 03:09:15 -0700

    Apps()

---
+++ heroku.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
+package heroku
+
+import (
+	"errors"
+	"github.com/jmcvetta/restclient"
+	"net/url"
+)
+
+const HerokuApi = "https://api.heroku.com"
+
+var BadResponse = errors.New("Bad response from server")
+
+// An Account is a Heroku user account
+type Account struct {
+	ApiKey string
+}
+
+// An App is a Heroku application.
+type App struct {
+	Id                int64
+	Name              string
+	CreateStatus      string `json:"create_status"`
+	CreatedAt         string `json:"created_at"`
+	Stack             string
+	RequestedStack    string `json:"requested_stack"`
+	RepoMigrateStatus string `json:"repo_migrate_status"`
+	SlugSize          int    `json:"slug_size"`
+	RepoSize          int    `json:"repo_size"`
+	Dynos             int
+	Workers           int
+}
+
+func (a *Account) userinfo() *url.Userinfo {
+	return url.UserPassword("", a.ApiKey)
+}
+
+// Apps queries Heroku for a list of all applications owned by account.
+func (a *Account) Apps() ([]*App, error) {
+	url := HerokuApi + "/apps"
+	res := new([]*App)
+	rr := restclient.RequestResponse{
+		Url:      url,
+		Method:   "GET",
+		Userinfo: a.userinfo(),
+		Result:   res,
+	}
+	status, err := restclient.Do(&rr)
+	if err != nil {
+		return nil, err
+	}
+	if status != 200 {
+		return nil, BadResponse
+	}
+	return *res, nil
+}
