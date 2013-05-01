888dd036c45a6f24da1ff37c4dc7cd8f82365047
Jason McVetta <jason.mcvetta@gmail.com>
2013-05-01 14:51:07 -0700

    Better GetApps() and test

--- heroku.go
+++ heroku.go
@@ -10,13 +10,25 @@ import (
 	"net/url"
 )
 
+const HerokuApi   = "https://api.heroku.com"
+
 var (
-	HerokuApi   = "https://api.heroku.com"
 	BadResponse = errors.New("Bad response from server")
 )
 
+func NewHeroku(apiKey string) *Heroku {
+	h := Heroku{
+		ApiKey: apiKey,
+		ApiHref: HerokuApi,
+		rc: restclient.New(),
+	}
+	return &h
+}
+
 type Heroku struct {
 	ApiKey string
+	ApiHref string
+	rc *restclient.Client
 }
 
 // An App is a Heroku application.
@@ -34,6 +46,9 @@ type App struct {
 	Workers           int
 }
 
+type mss map[string]string
+
+
 func (h *Heroku) userinfo() *url.Userinfo {
 	return url.UserPassword("", h.ApiKey)
 }
@@ -41,15 +56,17 @@ func (h *Heroku) userinfo() *url.Userinfo {
 // Apps queries Heroku for all applications owned by account, and returns a
 // map keyed with app IDs.
 func (h *Heroku) Apps() (map[int64]*App, error) {
-	url := HerokuApi + "/apps"
+	url := h.ApiHref + "/apps"
 	res := []*App{}
+	e := new(interface{})
 	rr := restclient.RequestResponse{
 		Url:      url,
 		Method:   "GET",
 		Userinfo: h.userinfo(),
 		Result:   &res,
+		Error: e,
 	}
-	status, err := restclient.Do(&rr)
+	status, err := h.rc.Do(&rr)
 	if err != nil {
 		return nil, err
 	}
@@ -61,4 +78,31 @@ func (h *Heroku) Apps() (map[int64]*App, error) {
 		m[a.Id] = a
 	}
 	return m, nil
+}
+
+func (h *Heroku) NewApp(name, stack string) (*App, error) {
+	url := h.ApiHref + "/apps"
+	a := new(App)
+	payload := struct {
+		App mss `json:"app"`
+	}{
+		App: mss{"name": name, "stack": stack,},
+	}
+	e := new(interface{})
+	rr := restclient.RequestResponse{
+		Url: url,
+		Method: "POST",
+		Userinfo: h.userinfo(),
+		Result: a,
+		Data: payload,
+		Error: e,
+	}
+	status, err := h.rc.Do(&rr)
+	if err != nil {
+		return nil, err
+	}
+	if status != 202 {
+		return nil, BadResponse
+	}
+	return a, nil
 }
--- heroku_test.go
+++ heroku_test.go
@@ -13,20 +13,24 @@ import (
 	"reflect"
 	"testing"
 	"time"
+	"log"
 )
 
-func setup(t *testing.T) *Heroku {
+func setup(t *testing.T, s *httptest.Server) *Heroku {
+	log.SetFlags(log.Lshortfile)
 	key := env.StringDefault("HEROKU_API_KEY", "")
 	if key == "" {
 		t.Fatal("HEROKU_API_KEY environment variable not set")
 	}
-	h := new(Heroku)
-	h.ApiKey = key
+	h := NewHeroku(key)
+	addr := "http://" + s.Listener.Addr().String()
+	h.ApiHref = addr
+	h.rc.UnsafeBasicAuth = true
 	return h
 }
 
-var testApps = []App{
-	App{
+var testApps = []*App{
+	&App{
 		Id:                1,
 		Name:              "foo",
 		CreateStatus:      time.Now().String(),
@@ -39,7 +43,7 @@ var testApps = []App{
 		Dynos:             3,
 		Workers:           1,
 	},
-	App{
+	&App{
 		Id:                2,
 		Name:              "bar",
 		CreateStatus:      time.Now().String(),
@@ -59,19 +63,33 @@ func HandleGetApps(w http.ResponseWriter, r *http.Request) {
 	enc.Encode(&testApps)
 }
 
+
+func HandleNewApp(w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+	a := testApps[0]
+	enc.Encode(&a)
+	w.WriteHeader(http.StatusAccepted)
+}
+
 func TestGetApps(t *testing.T) {
-	h := setup(t)
 	srv := httptest.NewServer(http.HandlerFunc(HandleGetApps))
 	defer srv.Close()
+	h := setup(t, srv)
 	m, err := h.Apps()
 	if err != nil {
 		t.Fatal(err)
 	}
-	for _, a0 := range m {
+	for _, a0 := range testApps {
 		a1, ok := m[a0.Id]
 		if !ok {
-			t.Error("Apps() failed to return app ", a0.Id)
+			t.Fatal("Apps() failed to return app ", a0.Id)
 		}
 		assert.T(t, reflect.DeepEqual(a0, a1))
 	}
+}
+
+func TestNewApp(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(HandleNewApp))
+	defer srv.Close()
+	// h := setup(t, srv)
 }
